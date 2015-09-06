61ca06adfae0d6af8f48795f0889bc7f4e39ba1d
ChanSteven <s3748679@gmail.com>
2015-09-06 16:11:17 +0800

    [add] split & add subtree

--- lab-subtree/build.sh
+++ lab-subtree/build.sh
@@ -2,10 +2,11 @@ mkdir -p repos
 rm -fr repos/*
 cd repos
 
-git init --bare repo-bare
-git clone repo-bare/ r1
+git init --bare repo-master-bare
+git init --bare repo-slave-bare
+git clone repo-master-bare/ repo-master
 
-cd r1
+cd repo-master
 echo f1 > f1.txt
 echo f2 > f2.txt
 
@@ -40,3 +41,17 @@ git commit -m "c5_f13"
 echo c6_f1 > f1.txt
 git add .
 git commit -m "c6_f1"
+
+git subtree split -P d1 -b export-d1
+git remote add slave ../repo-slave-bare
+git push slave export-d1:master
+cd ..
+
+git clone repo-slave-bare/ repo-slave
+cd repo-master
+
+git rm -r d1
+git commit -m "Remove split code."
+#git subtree add --prefix=d1 --squash slave master
+git subtree add -P d1 slave master 
+
