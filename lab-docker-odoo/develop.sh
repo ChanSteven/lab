0ae0e279e170e747d0adcb44e93d5c4c5f5d5228
ChanSteven <s3748679@gmail.com>
2015-10-30 01:20:55 +0800

    [update] odoo install success

--- lab-docker-odoo/develop.sh
+++ lab-docker-odoo/develop.sh
@@ -1,9 +1,11 @@
 docker rm -f lab-docker-odoo-db
 docker run -d -v $PWD/DATA:/var/lib/postgresql/data -e POSTGRES_USER=odoo -e POSTGRES_PASSWORD=odoo --name lab-docker-odoo-db postgres
 
+cp ./source/requirements.txt .
 docker rmi lab-docker-odoo
 docker build -t lab-docker-odoo .
 docker run --rm --name container-lab-docker-odoo \
            -v $PWD/source:/opt/odoo \
            --link lab-docker-odoo-db:db \
+           -p 8069:8069 \
            -i -t lab-docker-odoo /entrypoint-debug.sh
--- lab-docker-odoo/scripts/pre-setting.sh
+++ lab-docker-odoo/scripts/pre-setting.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+set -e
+
+# set odoo database host, port, user and password
+: ${PGHOST:=$DB_PORT_5432_TCP_ADDR}
+: ${PGPORT:=$DB_PORT_5432_TCP_PORT}
+: ${PGUSER:=${DB_ENV_POSTGRES_USER:='postgres'}}
+: ${PGPASSWORD:=$DB_ENV_POSTGRES_PASSWORD}
+export PGHOST PGPORT PGUSER PGPASSWORD
+
 adduser --system --quiet --shell=/bin/bash --home=$ODOO_HOME --gecos 'ODOO' --group $ODOO_USER
 chown -R $ODOO_USER:$ODOO_USER $ODOO_HOME
