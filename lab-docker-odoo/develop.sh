bce376a027388ad9da996a2a7e76fdac21da3b32
ChanSteven <s3748679@gmail.com>
2015-11-04 03:20:21 +0800

    [add] ODOO_VERSION & pyinotify for odoo8

--- lab-docker-odoo/develop.sh
+++ lab-docker-odoo/develop.sh
@@ -1,15 +1,23 @@
-docker rm -f lab-docker-odoo-db
-docker run -d -v $PWD/DATA:/var/lib/postgresql/data -e POSTGRES_USER=odoo -e POSTGRES_PASSWORD=odoo --name lab-docker-odoo-db postgres
+cd source
+ODOO_VERSION=$(git rev-parse --abbrev-ref HEAD)
+cd ..
+
+IMAGE_NAME=lab-docker-odoo-$ODOO_VERSION
+CONTAINER_NAME=$IMAGE_NAME-container
+DB_NAME=$IMAGE_NAME-db
+
+docker rm -f $DB_NAME
+docker run -d -v $PWD/DATA:/var/lib/postgresql/data -e POSTGRES_USER=odoo -e POSTGRES_PASSWORD=odoo --name $DB_NAME postgres
 
 cmd=/entrypoint.sh
 #cmd=/entrypoint-debug.sh
 
 cp ./source/requirements.txt .
-#docker rmi lab-docker-odoo
-docker build -t lab-docker-odoo .
-docker run --rm --name container-lab-docker-odoo \
+#docker rmi $IMAGE_NAME
+docker build -t $IMAGE_NAME .
+docker run --rm --name $CONTAINER_NAME \
            -v $PWD/source:/opt/odoo \
            -v $PWD/ODOO:/var/lib/odoo \
-           --link lab-docker-odoo-db:db \
+           --link $DB_NAME:db \
            -p 8069:8069 \
-           -i -t lab-docker-odoo $cmd
+           -i -t $IMAGE_NAME $cmd
